72455e24abac1149b634e5d7ec92d6f012fedb20
Medina Zeneli <mez001@edu.zealand.dk>
2025-09-20 22:56:35 +0200

    Første gang at jeg pusher - Test

---
+++ Bridge/Bridge/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    public class Car
+    {
+
+        public string Licenseplate { get; set; }
+        public DateTime Date { get; set; }
+
+
+
+        public double Price()
+        {
+            return 230;
+        }
+
+        public string VehicleType()
+        {
+            return "Car";
+        }
+    }
+}
---
+++ Bridge/BridgeTest1/Test1.cs
@@ -0,0 +1,25 @@
+﻿using Bridge;
+
+
+namespace BridgeTest
+{
+    /// <summary>
+    /// Unit test for Car Klassen. 
+    /// </summary>
+    [TestClass]
+    public class CarTest
+    {
+        [TestMethod]
+        public void VehicleType_ShouldReturnCar()
+        {
+            // Arrange 
+            var car = new Car();
+
+            //Act
+            var result = car.VehicleType();
+
+            //Assert
+            Assert.AreEqual("Car", result);
+        }
+    }
+}
